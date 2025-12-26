3a30c8baef0cc6ff2c9030c2d5d4c7657e958708
matthewlaneD <vxfxylia69431@outlook.com>
2025-12-26 17:12:14 +0800

    deploy: add vault + cUSDT deployment flow

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -3,15 +3,34 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
+  const { deploy, execute } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedCUSDT = await deploy("ConfidentialUSDT", {
     from: deployer,
+    args: [deployer, deployer],
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ConfidentialUSDT contract: `, deployedCUSDT.address);
+
+  const deployedVault = await deploy("CipherVault", {
+    from: deployer,
+    args: [deployedCUSDT.address],
+    log: true,
+  });
+
+  console.log(`CipherVault contract: `, deployedVault.address);
+
+  await execute(
+    "ConfidentialUSDT",
+    {
+      from: deployer,
+      log: true,
+    },
+    "setMinter",
+    deployedVault.address,
+  );
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_cipher_vault"; // id required to prevent reexecution
+func.tags = ["CipherVault", "ConfidentialUSDT"];
