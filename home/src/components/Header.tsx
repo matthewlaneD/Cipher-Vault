6f417918138d17f8d010fff7d0f7a7532becc438
matthewlaneD <vxfxylia69431@outlook.com>
2025-12-26 17:13:24 +0800

    frontend: build the header component

---
+++ home/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import '../styles/Header.css';
+
+export function Header() {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="header-content">
+          <div className="header-left">
+            <h1 className="header-title">
+              Cipher Vault
+            </h1>
+          </div>
+          <ConnectButton />
+        </div>
+      </div>
+    </header>
+  );
+}
